11a1ce6970e246b5c1421bc1e5e64f7ce147889c
Netson Cavina <netsoncavina@gmail.com>
2025-02-10 00:18:16 -0300

    add main game loop and quit functionality

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from sys import exit
+import config
+
+pygame.init()
+clock = pygame.time.Clock()
+
+def quit_game():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+def main():
+    while True:
+        quit_game()
+
+        config.window.fill((0, 0, 0))
+
+        clock.tick(60)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
