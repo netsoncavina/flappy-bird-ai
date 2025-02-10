36d3c072c07b152dac04f766aafb1c5ba9682d37
Netson Cavina <netsoncavina@gmail.com>
2025-02-10 00:31:34 -0300

    add pipe generation and rendering in the main game loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 import config
+import components
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -10,14 +11,30 @@ def quit_game():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+def generate_pipes():
+    config.pipes.append(components.Pipes(config.win_width))
 
 def main():
+    pipes_spawn_time = 10
+
     while True:
         quit_game()
 
         config.window.fill((0, 0, 0))
 
         config.ground.draw(config.window)
+
+        if pipes_spawn_time <= 0:
+            generate_pipes()
+            pipes_spawn_time = 200
+        pipes_spawn_time -= 1
+
+        for pipe in config.pipes:
+            pipe.draw(config.window)
+            pipe.update()
+            if pipe.off_screen:
+                config.pipes.remove(pipe)
+
 
         clock.tick(60)
         pygame.display.flip()
