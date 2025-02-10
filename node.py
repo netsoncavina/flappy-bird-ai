d18fcda1369c79e623beb4d15a22874d34423dfd
Netson Cavina <netsoncavina@gmail.com>
2025-02-10 02:16:40 -0300

    refactor Node class for improved readability and performance in activation method

--- node.py
+++ node.py
@@ -1,7 +1,8 @@
 import math
 
+
 class Node:
-    def __init__ (self, id_number):
+    def __init__(self, id_number):
         self.id = id_number
         self.layer = 0
         self.input_value = 0
@@ -10,16 +11,27 @@ class Node:
 
     def activate(self):
         def sigmoid(x):
-            return 1 / (1 + math.exp(-x))
+            return 1/(1+math.exp(-x))
 
         if self.layer == 1:
             self.output_value = sigmoid(self.input_value)
 
-        for connection in self.connections:
-            connection.to_node.input_value += self.output_value * connection.weight
+        for i in range(0, len(self.connections)):
+            self.connections[i].to_node.input_value += \
+                self.connections[i].weight * self.output_value
 
     def clone(self):
         clone = Node(self.id)
         clone.id = self.id
         clone.layer = self.layer
         return clone
+
+
+
+
+
+
+
+
+
+
